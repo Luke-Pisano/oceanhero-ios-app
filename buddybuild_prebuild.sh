7efdf8263205f4c80b4660b13d3c077e3db05b51
Cezary Bielecki <cezary.bielecki@digitalforms.pl>
2020-08-07 12:50:54 +0200

    Added swiftlint pre build buddybuild script. (#1)

---
+++ buddybuild_prebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  buddybuild_prebuild.sh
+#  DuckDuckGo
+#
+#  Created by Cezary Bielecki on 07/08/2020.
+#  Copyright © 2020 DuckDuckGo. All rights reserved.
+
+brew install swiftlint
